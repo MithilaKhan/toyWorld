a1377afb3bc567cf0bcfba1ca843e8c68abe9193
MithilaKhan <mithilakhan082@gmail.com>
2023-05-18 01:40:25 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+   return (
+      <div>
+        <h1 className='text-5xl font-bold text-purple-600'>this is home</h1> 
+      </div>
+   );
+};
+
+export default Home;
